7687ee4dde2ab9db851866238d12f297f7fe13f9
clans <jjgood12@naver.com>
2019-08-29 14:33:32 +0900

    first Commit

---
+++ src/screens/Memberinput.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+import Button from '../components/Button'
+
+export default class MemberInput extends Component{
+    render(){
+        return(
+            <div>
+                
+                <Button action={()=>{}}/>
+
+            </div>
+        )
+    }
+}
